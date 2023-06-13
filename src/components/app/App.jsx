06d4d4d2f1bfaeed48c36da88de208c13314068a
shokhruzisroilov <117011473+shohcoding@users.noreply.github.com>
2023-06-13 23:03:30 +0500

    End header

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,18 +1,18 @@
-import './App.scss';
-import {Routes, Route } from 'react-router-dom';
-import Layout from '../../layout/Layout';
-import { Navigate } from 'react-router-dom';
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from '../../layout/Layout'
+import { Navigate } from 'react-router-dom'
 
-import HomePage from '../../pages/HomePage';
-import Login from '../../login/Login';
-import Error404Page from '../../error/Error404Page';
+import HomePage from '../../pages/HomePage'
+import Login from '../../login/Login'
+import Error404Page from '../../error/Error404Page'
 
 function App() {
-  return (
+	return (
 		<>
 			<Routes>
 				{/* <Route path='/' element={<Navigate to={'/sign-in'} />} /> */}
-				<Route path='/home' element={<Layout />}>
+				<Route path='/' element={<Layout />}>
 					<Route index element={<HomePage />} />
 					{/* <Route path='/nimadur' element={nimadur}/> */}
 				</Route>
@@ -23,4 +23,4 @@ function App() {
 	)
 }
 
-export default App;
+export default App
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import './Header.scss'
+import { Link } from 'react-router-dom'
+
+import { CiTimer } from 'react-icons/ci'
+import { TbBrandTelegram } from 'react-icons/tb'
+import { TfiEmail } from 'react-icons/tfi'
+
 
 function Header() {
 	return (
-		<div>Header</div>
+		<div className='header'>
+			<div className='header__container'>
+				<div className='contact__info'>
+					<span>
+						<CiTimer className='header__icons' />
+						<p>Exchange 24/7</p>
+					</span>
+					<span>
+						<TbBrandTelegram className='header__icons' />
+						<p className='pragraf__hover'>Telegram (General questions)</p>
+					</span>
+					<span>
+						<TbBrandTelegram className='header__icons' />
+						<p className='pragraf__hover'>Telegram (Cash exchange)</p>
+					</span>
+					<span>
+						<TfiEmail className='header__icons' />
+						<p>info@kingex.io</p>
+					</span>
+					<span>
+						<TbBrandTelegram className='header__icons' />
+						<p className='pragraf__hover'>AML/KYC</p>
+					</span>
+				</div>
+				<div className='navbar'>
+					<div className='site__logo'>
+						<h1>Kingex</h1>
+					</div>
+					<nav>
+						<ul>
+							<li>
+								<Link>Partners</Link>
+							</li>
+							<li>
+								<Link>Exchange rules</Link>
+							</li>
+							<li>
+								<Link>FAQ</Link>
+							</li>
+							<li>
+								<Link>Contacts</Link>
+							</li>
+						</ul>
+					</nav>
+					<div className='nav__log'>
+						<Link className='login'>Login</Link>
+						<Link className='registr'>Registration</Link>
+					</div>
+				</div>
+			</div>
+		</div>
 	)
 }
 
